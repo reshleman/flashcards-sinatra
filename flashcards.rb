defa630b59218310dc5daeeebc994723c5912f6a
Robert Eshleman <c.robert.eshleman@gmail.com>
2014-06-27 11:22:02 -0400

    Show list of decks on root route

--- flashcards.rb
+++ flashcards.rb
@@ -1,5 +1,19 @@
 require "sinatra"
+require "active_record"
+
+ActiveRecord::Base.establish_connection(
+  adapter: "postgresql",
+  database: "flashcard-sinatra"
+)
+
+class Deck < ActiveRecord::Base
+  has_many :cards
+end
+
+class Card < ActiveRecord::Base
+end
 
 get "/" do
+  @decks = Deck.order("name ASC")
   erb :index
 end
