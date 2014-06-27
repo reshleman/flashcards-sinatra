e5eeb429db553bbd0d63c2d786262071f3b2c0eb
Robert Eshleman <c.robert.eshleman@gmail.com>
2014-06-27 11:54:26 -0400

    Minor code cleanup for readability

--- flashcards.rb
+++ flashcards.rb
@@ -13,31 +13,37 @@ end
 class Card < ActiveRecord::Base
 end
 
+# GET index
 get "/" do
   @decks = Deck.order("name ASC")
   erb :index
 end
 
+# GET new deck form
 get "/decks/new" do
   erb :new_deck
 end
 
+# POST new deck
+post "/decks" do
+  @deck = Deck.create(params["deck"])
+  redirect to("/decks/#{@deck.id}")
+end
+
+# GET view for a deck
 get "/decks/:deck_id" do
   @deck = Deck.find(params[:deck_id])
   @cards = @deck.cards
   erb :show_deck
 end
 
-post "/decks" do
-  @deck = Deck.create(params["deck"])
-  redirect to("/decks/#{@deck.id}")
-end
-
+# GET new card form
 get "/decks/:deck_id/cards/new" do
   @deck = Deck.find(params[:deck_id])
   erb :new_card
 end
 
+# POST new card
 post "/decks/:deck_id/cards" do
   @card = Card.create(params[:card])
   redirect to("/decks/#{@card.deck_id}")
